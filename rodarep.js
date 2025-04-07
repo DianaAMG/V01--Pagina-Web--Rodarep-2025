e0d747cfd2cba4f4d755532183424ad7c07071b4
diana <dianaamonroy@gmail.com>
2025-04-07 15:14:53 -0500

    Se realizó ajuste en diseño Front de Form , y logica para mostrar en consola datos ingresados al formulario

--- rodarep.js
+++ rodarep.js
@@ -2,3 +2,21 @@
 document.getElementById('measure__block--bearings').addEventListener('click', function () {
     window.location.href = 'medidor.html#meter__calculator-bearing';
 });
+
+const inputName= document.getElementById('name')
+const email= document.getElementById('email')
+const message= document.getElementById('message')
+const button = document.getElementById('contact-us__button')
+
+button.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    const data = {
+        inputName:inputName.value,
+        email:email.value,
+        message: message.value  
+    } 
+    location.reload();
+    
+
+    console.log(data);
+});
