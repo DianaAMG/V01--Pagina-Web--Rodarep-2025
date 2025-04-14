1cd67574b2d6a037f95d9756a1e2f86b54d75e10
diana <dianaamonroy@gmail.com>
2025-04-14 16:54:49 -0500

    Se desarrolló conexión de datos del visitante con rodamientos en JS  y funcionalidad del  botón de cotizar a WhatsApp en el HTML

---
+++ constantes.js
@@ -0,0 +1,4 @@
+export const bearings = [
+    {ref:6203, d: 17, D: 40, B:12, },
+    {ref:6300, d: 10, D: 35, B:11, },
+]
--- medidor.js
+++ medidor.js
@@ -1,14 +1,35 @@
-document.getElementById('button__meter-measure').addEventListener('click', function(event){
+document
+  .getElementById("button__meter-measure")
+  .addEventListener("click", function (event) {
     event.preventDefault();
 
-    const d = parseInt(document.getElementById('d__internal').value);
-    const D = parseInt(document.getElementById('D__external').value); 
-    const B = parseInt(document.getElementById('B__width').value);
+    const d = parseInt(document.getElementById("d__internal").value);
+    const D = parseInt(document.getElementById("D__external").value);
+    const B = parseInt(document.getElementById("B__width").value);
 
-    console.log('el diametro interno del rodamiento es ', d);
-    console.log('el diametro externo del rodamiento es ', D);
-    console.log('el ancho del rodamiento es ', B);
+    const bearings = [
+      { ref: 6203, d: 17, D: 40, B: 12, info: "Información de tu rodamiento" },
+      { ref: 6300, d: 10, D: 35, B: 11 },
+    ];
 
+    const informationsBearings = bearings.find(
+      (rodamiento) =>
+        rodamiento.d === d && rodamiento.D === D && rodamiento.B === B
+    );
 
+    if (informationsBearings) {
+      document.getElementById("meter__container-result-text").innerHTML =
+        informationsBearings.ref;
+      document.getElementById("meter__calculator-information-text").innerHTML =
+        informationsBearings.info;
+    } else {
+      document.getElementById("meter__container-result-text").innerHTML =
+        "Sin Resultado";
+      document.getElementById("meter__calculator-information-text").innerHTML =
+        "No se encontró información";
+    }
 
-})
+    document.getElementById("d__internal").value = "";
+    document.getElementById("D__external").value = "";
+    document.getElementById("B__width").value = "";
+  });
