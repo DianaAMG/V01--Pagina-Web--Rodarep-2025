a9316fdaa200a96274a2067864d13ff417aa39d5
diana <dianaamonroy@gmail.com>
2025-04-14 21:10:02 -0500

    Se creó nuevo JS con arrays de rodamientos, se usó exports e imports para relacionar  y de realizó el  enlace  del botón de cotizar por WhatsApp con mensaje según referencia encontrada que requiere el cliente

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
- function myFunction() {
-    window.location.href = 'medidor.html#meter__containerg';
+function function__bearings() {
+  window.location.href = "medidor.html#meter__container";
 }
 
-document.getElementById('contact-us__button').addEventListener('click', function (event) {
+document
+  .getElementById("contact-us__button")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-  
-    const form = document.getElementById('form');
+
+    const form = document.getElementById("form");
     const formData = new FormData(form);
-  
-    const nameValue = formData.get('name');
-    const phonevalue = formData.get('phone');
-    const emailValue = formData.get('email');
-    const messageValue = formData.get('message');
-  
+
+    const nameValue = formData.get("name");
+    const phonevalue = formData.get("phone");
+    const emailValue = formData.get("email");
+    const messageValue = formData.get("message");
+
     const subject = encodeURIComponent("Nuevo mensaje de contacto");
     const body = encodeURIComponent(
       `Nombre: ${nameValue}\nTeléfono: ${phonevalue}\nCorreo: ${emailValue}\n\nMensaje:\n${messageValue}`
     );
-  
+
     const mailtoLink = `mailto:dianaamonroy@gmail.com?subject=${subject}&body=${body}`;
     window.open(mailtoLink);
 
-     form.reset();
-
-})
-
-
-
+    form.reset();
+  });
--- medidor.js
+++ medidor.js
@@ -1,3 +1,5 @@
+import bearings from "./constantes.js";
+
 document
   .getElementById("button__meter-measure")
   .addEventListener("click", function (event) {
@@ -6,11 +8,6 @@ document
     const d = parseInt(document.getElementById("d__internal").value);
     const D = parseInt(document.getElementById("D__external").value);
     const B = parseInt(document.getElementById("B__width").value);
-
-    const bearings = [
-      { ref: 6203, d: 17, D: 40, B: 12, info: "Información de tu rodamiento" },
-      { ref: 6300, d: 10, D: 35, B: 11 },
-    ];
 
     const informationsBearings = bearings.find(
       (rodamiento) =>
@@ -32,4 +29,15 @@ document
     document.getElementById("d__internal").value = "";
     document.getElementById("D__external").value = "";
     document.getElementById("B__width").value = "";
+
+    document
+      .getElementById("button__quote")
+      .addEventListener("click", function () {
+        window.open(
+          `https://api.whatsapp.com/send?phone=573167437807&text=Hola,%20quiero%20conocer%20el%20valor%20del%20rodamiento%20REF:%20${encodeURIComponent(
+            informationsBearings.ref
+          )}`,
+          "_blank"
+        );
+      });
   });
